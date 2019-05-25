47b8446445a2808d6eede951a7daf1e618162fff
t-eckert <t_eckert@outlook.com>
2019-05-25 15:43:15 -0700

    First draft of Site

---
+++ application.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template, url_for
+from .post_catcher import posts
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("home.html", posts=posts)
+
